ad19bfef8e8b5f88468064c515e3f823de21a2f3
Jesus2508 <165663442+Jesus2508@users.noreply.github.com>
2024-10-28 20:15:31 +0000

    entregar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document
+  .getElementById("generarExcusaBoton")
+  .addEventListener("click", generarExcusa);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarExcusa() {
+  const who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  const action = ["se comió", "orinó", "aplastó", "rompió"];
+  const what = ["mis deberes", "mi teléfono", "el coche"];
+  const when = [
+    "antes de la clase",
+    "mientras dormía",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  const excusa =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)] +
+    ".";
+
+  document.getElementById("excuse").innerHTML = excusa;
+}
+
+window.onload = generarExcusa;
